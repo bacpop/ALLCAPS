25159c2f01f51e4e9cfcdc6dd33d6a85a03ce5a7
Alireza Tajmirriahi <tajmirri@ebi.ac.uk>
2025-05-29 12:05:46 +0100

    Add Dataset and Module to models

--- 1_data_annotation/scripts/models.py
+++ 1_data_annotation/scripts/models.py
@@ -1,8 +1,11 @@
+import os
+import numpy as np
+import pandas as pd
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import Dataset
-import numpy as np
-from transformers import AutoModel
 
 
 class ContrastiveHead(nn.Module):
@@ -19,60 +22,38 @@ class ContrastiveHead(nn.Module):
 
 
 class ContrastiveChunkedDataset(Dataset):
-    def __init__(self, embeddings_path, capsule_labels, serotype_labels):
+    def __init__(self, embeddings_dir, sampled_ids, serotype_labels, capsule_labels):
         """
-        embeddings_path: str, path to .pt or .npz with {'sample_id': [L, D]} entries
-        capsule_labels: list of capsule labels (binary)
-        serotype_labels: list of serotype labels
+        embeddings_path: str, path to directory with npy entries of variable length chunked embeddings
+        serotype_labels: pd.DataFrame, DataFrame with serotype labels indexed by sample ID.
         """
-        if embeddings_path.endswith(".pt"):
-            self.embeddings = torch.load(embeddings_path)
-        elif embeddings_path.endswith(".npz"):
-            self.embeddings = {k: torch.tensor(v) for k, v in np.load(embeddings_path, allow_pickle=True).items()}
-        else:
-            raise ValueError("Unsupported format. Use .pt or .npz")
+        self.embedding_dir = embeddings_dir
+        self.sample_ids = sampled_ids
+        self.serotypes = serotype_labels["Serotype"].astype(str).to_list()
+        self.is_capsule = capsule_labels 
+        self.is_capsule = (serotype_labels["Serotype"] != "NON-CBL").astype(int).to_list()
 
-        self.capsule_labels = capsule_labels
-        self.serotype_labels = serotype_labels
-        assert len(self.embeddings) == len(capsule_labels) == len(serotype_labels), \
-            "Mismatch in number of samples between embeddings and labels."
+        assert serotype_labels.index.isin([f.split(".")[0] for f in os.listdir(embeddings_dir) if f.endswith('.npy')]).all(), \
+            "At least one sample in serotype_labels does not have a corresponding embedding file."
 
-    def __len__(self):
-        return len(self.embeddings)
-
+    def __len__(self) -> int:
+        return len(self.sample_ids)
+    
     def __getitem__(self, idx):
+        embedding_path = os.path.join(self.embedding_dir, f"{self.sample_ids[idx]}.npy")
+        # if not os.path.exists(embedding_path):
+        #     raise FileNotFoundError(f"Embedding file not found at {embedding_path}")
         return {
-            'embedding': self.embeddings[idx],
-            'is_capsule': self.capsule_labels[idx],
-            'serotype': self.serotype_labels[idx]
+            'embedding': torch.tensor(np.load(embedding_path), dtype=torch.float32),
+            'serotype': self.serotypes[idx],
+            'is_capsule': self.is_capsule[idx]
         }
 
 
-class TransformerCapsuleClassifier(nn.Module):
-    def __init__(self, transformer_name, extra_layers=2, num_heads=8, hidden_dim=128):
-        super().__init__()
-        self.transformer = AutoModel.from_pretrained(transformer_name)
-        encoder_layer = nn.TransformerEncoderLayer(
-            d_model=self.transformer.config.hidden_size,
-            nhead=num_heads,
-            dim_feedforward=hidden_dim,
-            dropout=0.1
-        )
-        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=extra_layers)
-        self.classifier = nn.Linear(self.transformer.config.hidden_size, 2)
-
-    def forward(self, input_ids, attention_mask):
-        embeddings = self.transformer(input_ids, attention_mask).last_hidden_state
-        encoded_emb = self.encoder(embeddings.permute(1,0,2)).permute(1,0,2)
-        pooled_emb = encoded_emb.mean(dim=1)
-        logits = self.classifier(pooled_emb)
-        return logits, pooled_emb
-
-
 class TransformerContrastiveHead(nn.Module):
-    def __init__(self, input_dim, output_dim=128, max_len=128, nhead=4, num_layers=2):
+    def __init__(self, input_dim, output_dim=128, max_len=64, nhead=4, num_layers=2):
         super().__init__()
-        self.pos_embed = nn.Embedding(max_len, input_dim)
+        self.pos_embed = nn.Embedding(max_len, input_dim)  # TODO Dynamically expand or clamp
 
         encoder_layer = nn.TransformerEncoderLayer(
             d_model=input_dim,
@@ -81,6 +62,7 @@ class TransformerContrastiveHead(nn.Module):
             batch_first=True
         )
         self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
+        self.classifier = nn.Linear(input_dim, 2)
 
         self.project = nn.Sequential(
             nn.Linear(input_dim, input_dim),
@@ -92,6 +74,7 @@ class TransformerContrastiveHead(nn.Module):
         B, L, D = x.size()
         pos = torch.arange(L, device=x.device).unsqueeze(0)  # (1, L)
         x = x + self.pos_embed(pos)
-        x = self.encoder(x)
-        x = x.mean(dim=1)
-        return F.normalize(self.project(x), dim=1)
+        x = self.encoder(x)  # Encoded (B, L, D)
+        x = x.mean(dim=1)  # Pooled (B, D)
+        logits = self.classifier(x)  # Classifier output (B, output_dim)
+        return logits, F.normalize(self.project(x), dim=1)
